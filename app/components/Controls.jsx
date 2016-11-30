0ee337710f6aac3f0d0ec473d4def9fb5b90b177
bahram <bsamani@samani.com>
2016-11-30 17:52:28 -0500

    update status to stop when count reaches zero

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -13,6 +13,9 @@ var Controls = React.createClass ({
       this.props.onStatusChange(newStatus);
     }
   },
+  componentWillReceiveProps: function (newProps){
+    console.log('componentWillReceiveProps', newProps.countdownStatus);
+  },
   render: function () {
     var {countdownStatus} = this.props;
     var renderStopStartButton = () => {
